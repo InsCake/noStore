156994d39c2dd02615f635327c1641c2097907a6
InsCake <m.saihomi@gmail.com>
2014-03-26 21:45:38 +0800

    0.1.0
    基本构架实现
    首页
    详情页
    列表页

--- app.js
+++ app.js
@@ -56,14 +56,17 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', routes.home);
+app.get('/detail/:id', routes.detail);
+app.get('/search', routes.search);
+app.get('/topic/:id', routes.topic);
 app.all('/login', notAuthentication);
 app.get('/login', routes.login);
 app.post('/login', routes.doLogin);
 app.get('/logout', authentication);
 app.get('/logout', routes.logout);
 app.get('/home', authentication);
-app.get('/home', routes.home);
+app.get('/home', routes.home_page);
 app.get('/movie/add', movie.movieAdd); //增加
 app.post('/movie/add', movie.doMovieAdd); //提交
 app.get('/movie/:name', movie.movieAdd); //编辑查询
@@ -90,21 +93,21 @@ http.createServer(app).listen(app.get('port'), function() {
 });
 
 // database
-var conn = mysql.createConnection(mysql_config.conn_config);
-conn.connect();
+// var conn = mysql.createConnection(mysql_config.conn_config);
+// conn.connect();
 
-var insertSQL = 'insert into user (name) values ("conan"),("fens.me")';
-var selectSQL = 'select * from user limit 10';
-var deleteSQL = 'delete from user';
-var updateSQL = 'update user set name="conan update" where name="conan"';
+// var insertSQL = 'insert into user (name) values ("conan"),("fens.me")';
+// var selectSQL = 'select * from user limit 10';
+// var deleteSQL = 'delete from user';
+// var updateSQL = 'update user set name="conan update" where name="conan"';
 
-conn.query(selectSQL, function(err2, rows) {
-    if (err2) console.log(err2);
+// conn.query(selectSQL, function(err2, rows) {
+//     if (err2) console.log(err2);
 
-    console.log("SELECT ==> ");
-    for (var i in rows) {
-        console.log(rows[i]);
-    }
-});
+//     console.log("SELECT ==> ");
+//     for (var i in rows) {
+//         console.log(rows[i]);
+//     }
+// });
 
 //conn.end();
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,138 @@
 var mysql = require('mysql');
 var mysql_config = require('./../models/mysql_config');
 
+exports.search = function(req, res) {
+    // mysql connection
+    var conn = mysql.createConnection(mysql_config.conn_config);
+    conn.connect();
+    var options = {
+        sql: "SELECT * FROM commodity_has_topic " + 
+        " INNER JOIN commodity ON commodity_has_topic.commodity_id = commodity.id " + 
+        " INNER JOIN seller on commodity.seller_id = seller.id " + 
+        " INNER JOIN commodity_has_tag ON commodity.id = commodity_has_tag.commodity_id " + 
+        " INNER JOIN tag ON commodity_has_tag.tag_id = tag.id " + 
+        " WHERE commodity.name LIKE '%" + req.query.key + "%' " + 
+        " OR commodity.subtitle LIKE '%" + req.query.key + "%' " + 
+        " OR commodity.category LIKE '%" + req.query.key + "%' " + 
+        " OR tag.name LIKE '%" + req.query.key + "%' ",
+        nestTables: '_'
+    };
+    console.log(req.query.key);
+    conn.query(options, function(err, rows) {
+        if (err) console.log(err);
+        if (rows.length > 0) {
+            var commoditys = rows;
+            console.log(commoditys);
+            res.render('list', {
+                'title': 'Express',
+                'commoditys': commoditys
+            });
+        } else { //无结果操作
+        }
+        conn.end();
+    });
+};
+exports.topic = function(req, res) {
+    // mysql connection
+    var conn = mysql.createConnection(mysql_config.conn_config);
+    conn.connect();
+    var options = {
+        sql: "SELECT * FROM commodity_has_topic INNER JOIN commodity ON commodity_has_topic.commodity_id = commodity.id INNER JOIN seller on commodity.seller_id = seller.id WHERE commodity_has_topic.topic_id = '" + req.params.id + "'",
+        nestTables: '_'
+    };
+    conn.query(options, function(err, rows) {
+        if (err) console.log(err);
+        if (rows.length > 0) {
+            var commoditys = rows;
+            console.log(commoditys);
+            res.render('list', {
+                'title': 'Express',
+                'commoditys': commoditys
+            });
+        } else { //无结果操作
+        }
+        conn.end();
+    });
+};
+exports.home = function(req, res) {
+    // mysql connection
+    var conn = mysql.createConnection(mysql_config.conn_config);
+    conn.connect();
+    var options = {
+        sql: "SELECT * FROM commodity INNER JOIN seller ON commodity.seller_id = seller.id",
+        nestTables: '_'
+    };
+    conn.query(options, function(err, rows) {
+        if (err) console.log(err);
+        if (rows.length > 0) {
+            var commoditys = rows;
+            console.log(rows[0]);
+            res.render('home_real', {
+                'title': 'Express',
+                'commoditys': commoditys
+            });
+        } else { //无结果操作
+        }
+        conn.end();
+    });
+};
+exports.detail = function(req, res) {
+    // mysql connection
+    var conn = mysql.createConnection(mysql_config.conn_config);
+    conn.connect();
+    var options_commodity = {
+        sql: "SELECT * FROM commodity INNER JOIN seller ON commodity.seller_id = seller.id WHERE commodity.id = '" + req.params.id + "'",
+        nestTables: '_'
+    };
+    var options_tags = {
+        sql: "SELECT * FROM commodity_has_topic INNER JOIN topic ON commodity_has_topic.commodity_id = topic.id WHERE commodity_has_topic.commodity_id = '" + req.params.id + "'",
+        nestTables: '_'
+    };
+    var options_likes = {
+        sql: "SELECT COUNT(*) AS likes_count FROM user_likes_commodity WHERE user_likes_commodity.commodity_id = '" + req.params.id + "'",
+        nestTables: '_'
+    };
+    // 查询商品信息    
+    conn.query(options_commodity, function(err, rows) {
+        if (err) console.log(err);
+        if (rows.length > 0) {
+            var commodity = rows[0];
+            console.log('SELECT commodity:');
+            console.log(commodity);
+            // 查询tag
+            conn.query(options_tags, function(err, rows) {
+                if (err) console.log(err);
+                if (rows.length > 0) {
+                    var topics = rows;
+                    console.log('SELECT topics:');
+                    console.log(topics);
+                    // 查询likes_count
+                    conn.query(options_likes, function(err, rows) {
+                        if (err) console.log(err);
+                        if (rows.length > 0) {
+                            var likes_count = rows[0]['_likes_count'];
+                            console.log('SELECT likes_count:');
+                            console.log(likes_count);
+                            res.render('detail', {
+                                'title': 'Express',
+                                'commodity': commodity,
+                                'topics': topics,
+                                'likes_count': likes_count
+                            });
+                            conn.end();
+                        } else { //无结果操作
+                            conn.end();
+                        }
+                    });
+                } else { //无结果操作
+                    conn.end();
+                }
+            });
+        } else { //无结果操作
+            conn.end();
+        }
+    });
+};
 exports.index = function(req, res) {
     res.render('index', {
         title: 'Express'
@@ -39,7 +171,7 @@ exports.logout = function(req, res) {
     req.session.user = null;
     res.redirect('/');
 };
-exports.home = function(req, res) {
+exports.home_page = function(req, res) {
     res.render('home', {
         title: 'Home'
     });
